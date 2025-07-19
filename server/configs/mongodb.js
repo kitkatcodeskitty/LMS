cfbae16c271692a5bdbf8e475840731f238f99b1
Dilan <dilandhakal889@gmail.com>
2025-07-19 15:45:52 +0545

    fixed typo

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,11 @@
-import mongoose, { mongo, MongooseError } from "mongoose";
-
-// connect to the mongoDB database
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log('Database connected'))
+  mongoose.connection.on("connected", () =>
+    console.log("Database connected")
+  );
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/App`)
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}/App`);
+};
 
-export default connectDB
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,24 @@
-import express from "express"
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clearWebhooks } from "./controllers/webhooks.js";
 
-// Express
 const app = express();
 
+// Connect to MongoDB
+await connectDB();
 
-// Database connection
-await connectDB()
-
-// middlewares
+// Middleware
 app.use(cors());
+app.use(express.json());
 
-// routes
-app.get('/',(req,res)=> res.send('Api working'));
-app.post('/clerk', express.json(), clearWebhooks);
+// Routes
+app.get("/", (req, res) => res.send("Api working"));
+app.post("/clerk", clearWebhooks);
 
-// Port number
-const PORT = process.env.PORT || 5000 
-
-app.listen(PORT, ()=>{
-    console.log(`Server is Running on port ${PORT}`)
-})
-
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
+});
