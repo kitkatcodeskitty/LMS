35d1990ecc8b184abde75883590736fa90e8c158
Dilan <dilandhakal889@gmail.com>
2025-07-22 09:05:55 +0545

    add code

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -35,7 +35,7 @@ export const clerkWebhooks = async (req, res) => {
       case "user.updated": {
         const userData = {
           email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
+          name: data.first_name + "" + data.last_name,
           imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, userData);
--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connecDB from './configs/mongodb.js'
+import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 // initialize express
 const app = express()
 
 // connect to db
-await connecDB()
+await connectDB()
 
 // middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 // route
 app.get('/', (req,res)=> res.send("api working"))
 app.post('/clerk',express.json(), clerkWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
+
 
 // port number
 const PORT = process.env.PORT || 5000 
